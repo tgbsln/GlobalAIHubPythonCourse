6cce499a80b40a1907f35652d55524b50ee75c7b
tgbsln <69511917+tgbsln@users.noreply.github.com>
2021-04-09 09:28:46 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,14 +1,14 @@
-d={"name":"John","pass":"12345"}
-n=d["name"]
-p=d["pass"]
-username = input('Enter username: ')
-password = input('Enter password: ')
+d={"name":"John","pass":"12345"} #dictionary oluşturuldu
+n=d["name"] #key element ile isim değişken bir değere atandı.
+p=d["pass"] # "      "   şifre   "    "      "      "
+username = input('Enter username: ') #kullanıcıdan ismini girmesi istenir.
+password = input('Enter password: ') #   "         şifresini  "      "
 
-if (username!= n and password==p):
-    print("Kullanıcı adınız hatalı")
-elif (username==n and password!= p):
+if (username!= n and password==p):     #eğer kullanıcı adı hatalı ve şifre doğruysa alttaki print edilir.
+    print("Kullanıcı adınız hatalı")   
+elif (username==n and password!= p):   #eğer kullanıcı adı doğru ve şifre yanlışsa alttaki print edilir.
     print("Password hatalı")
-elif (username!=n and password!= p):
+elif (username!=n and password!= p):   #eğer kullanıcı adı ve şifre hatalı ise       "        "    " 
     print("Username ve password hatalıdır.")
-else:
+else:                                   #ikiside doğruysa başarılı giriş yapılır.
     print("Tebrikler, Başarıyla giriş yaptınız")
