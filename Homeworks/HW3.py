009dd6a0c834eca337d1a131486d923445874d26
tgbsln <69511917+tgbsln@users.noreply.github.com>
2021-04-11 00:43:28 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,10 +1,10 @@
-students={"Tuğba":["Aslan",68,70,80],
+students={"Tuğba":["Aslan",68,70,80], 
           "Ceren":["Akar",75,80,90],
           "Fatoş":["Yılmaz",78,65,75],
           "Yıldız":["Kılıç",65,80,95],
-          "Zeynep":["Alşan",45,75,85]}
+          "Zeynep":["Alşan",45,75,85]}  #Created student information with dictionary
 Tuğba_midterm=students["Tuğba"][1]
-Tuğba_midterm
+Tuğba_midterm                            #Grades are taken by indexing separately for each student
 
 Tuğba_project=students["Tuğba"][2]
 Tuğba_project
@@ -15,7 +15,7 @@ Tuğba_final
 per1=0.3
 per2=0.3
 per3=0.4
-Tuğba_passgrade=(Tuğba_midterm*per1)+(Tuğba_project*per2)+(Tuğba_project*per3)
+Tuğba_passgrade=(Tuğba_midterm*per1)+(Tuğba_project*per2)+(Tuğba_project*per3)  #For all students, passgrade are calculated via formula
 Tuğba_passgrade
 
 Ceren_midterm=students["Ceren"][1]
@@ -66,24 +66,22 @@ Zeynep_final
 Zeynep_passgrade=(Zeynep_midterm*per1)+(Zeynep_project*per2)+(Zeynep_project*per3)
 Zeynep_passgrade
 
-studentsgrade_list=[]
-studentsgrade_list.extend((Tuğba_passgrade,Ceren_passgrade,Fatoş_passgrade,Yıldız_passgrade,Zeynep_passgrade))
+studentsgrade_list=[]   # list are created
+studentsgrade_list.extend((Tuğba_passgrade,Ceren_passgrade,Fatoş_passgrade,Yıldız_passgrade,Zeynep_passgrade)) # grades are added to list
 studentsgrade_list
-studentsgrade_list.sort(reverse=True);
-print(studentsgrade_list);
 
-students.keys()
+students.keys() #keys are selected for student names as a dict
 
 
-list(students.keys())
-names=list(students.keys())
+list(students.keys()) #dict is turned to list
+names=list(students.keys()) #list assigned to a variable
 names
 
-info=input("Please enter your name:")
+info=input("Please enter your name:") #ask for user name
 
-for i in range(len(names)):
+for i in range(len(names)):  # According to user input, grades are shown.
     if info==names[i]:
        print('Your passing grade is: ', studentsgrade_list[i])
        
-studentsgrade_list.sort(reverse=True);
+studentsgrade_list.sort(reverse=True); # grades have ascending order.
 print(studentsgrade_list)
