ad0ce83d31d8633c2b748d544c15a03f45416e7c
tgbsln <69511917+tgbsln@users.noreply.github.com>
2021-04-11 00:34:39 +0300

    Add files via upload

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,89 @@
-#Explain your work
+students={"Tuğba":["Aslan",68,70,80],
+          "Ceren":["Akar",75,80,90],
+          "Fatoş":["Yılmaz",78,65,75],
+          "Yıldız":["Kılıç",65,80,95],
+          "Zeynep":["Alşan",45,75,85]}
+Tuğba_midterm=students["Tuğba"][1]
+Tuğba_midterm
 
-#Question 1
-for x in range(a):
-	print(a)
+Tuğba_project=students["Tuğba"][2]
+Tuğba_project
+
+Tuğba_final=students["Tuğba"][3]
+Tuğba_final
+
+per1=0.3
+per2=0.3
+per3=0.4
+Tuğba_passgrade=(Tuğba_midterm*per1)+(Tuğba_project*per2)+(Tuğba_project*per3)
+Tuğba_passgrade
+
+Ceren_midterm=students["Ceren"][1]
+Ceren_midterm
+
+Ceren_project=students["Ceren"][2]
+Ceren_project
+
+Ceren_final=students["Ceren"][3]
+Ceren_final
+
+Ceren_passgrade=(Ceren_midterm*per1)+(Ceren_project*per2)+(Ceren_project*per3)
+Ceren_passgrade
+
+Fatoş_midterm=students["Fatoş"][1]
+Fatoş_midterm
+
+
+Fatoş_project=students["Fatoş"][2]
+Fatoş_project
+
+Fatoş_final=students["Fatoş"][3]
+Fatoş_final
+
+Fatoş_passgrade=(Fatoş_midterm*per1)+(Fatoş_project*per2)+(Fatoş_project*per3)
+Fatoş_passgrade
+
+Yıldız_midterm=students["Yıldız"][1]
+Yıldız_midterm
+
+Yıldız_project=students["Yıldız"][2]
+Yıldız_project
+Yıldız_final=students["Yıldız"][3]
+Yıldız_final
+
+Yıldız_passgrade=(Yıldız_midterm*per1)+(Yıldız_project*per2)+(Yıldız_project*per3)
+Yıldız_passgrade
+
+Zeynep_midterm=students["Zeynep"][1]
+Zeynep_midterm
+
+Zeynep_project=students["Zeynep"][2]
+Zeynep_project
+
+Zeynep_final=students["Zeynep"][3]
+Zeynep_final
+
+Zeynep_passgrade=(Zeynep_midterm*per1)+(Zeynep_project*per2)+(Zeynep_project*per3)
+Zeynep_passgrade
+
+studentsgrade_list=[]
+studentsgrade_list.extend((Tuğba_passgrade,Ceren_passgrade,Fatoş_passgrade,Yıldız_passgrade,Zeynep_passgrade))
+studentsgrade_list
+studentsgrade_list.sort(reverse=True);
+print(studentsgrade_list);
+
+students.keys()
+
+
+list(students.keys())
+names=list(students.keys())
+names
+
+info=input("Please enter your name:")
+
+for i in range(len(names)):
+    if info==names[i]:
+       print('Your passing grade is: ', studentsgrade_list[i])
+       
+studentsgrade_list.sort(reverse=True);
+print(studentsgrade_list)
